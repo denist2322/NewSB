fca33829cbe3e22cc7665b573360f39cd59b31f7
denist2322 <parksomang125@gmail.com>
2022-07-08 19:17:57 +0900

    존재하지 않는 게시물 입니다.

--- src/main/java/com/mysite/sbb/controller/ArticleController.java
+++ src/main/java/com/mysite/sbb/controller/ArticleController.java
@@ -5,6 +5,7 @@ import com.mysite.sbb.domain.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -25,5 +26,77 @@ public class ArticleController {
     @ResponseBody
     public List<Article> showList(){
         return articleRepository.findAll();
+    }
+
+    @RequestMapping("/get")
+    @ResponseBody
+    public Article getArticle(Long id){
+        Article article = articleRepository.findById(id).get();
+
+        return article;
+    }
+
+    @RequestMapping("/doModify")
+    @ResponseBody
+    public String doModify(Long id, String title, String body){
+        if(id == null){
+            return "id를 입력하세요.";
+        }
+
+        if(title == null){
+            return "title을 입력하세요.";
+        }
+
+        if(body == null) {
+            return "body를 입력하세요.";
+        }
+
+        Article article = articleRepository.findById(id).get();
+
+        article.setTitle(title);
+        article.setBody(body);
+
+        articleRepository.save(article);
+
+        return String.format("%d번 수정이 완료되었습니다.",id);
+    }
+
+    @RequestMapping("/doDelete")
+    @ResponseBody
+    public String doDelete(Long id){
+        if(!articleRepository.existsById(id)){
+            return "%d번 게시물은 이미 삭제되었거나 존재하지 않습니다.".formatted(id);
+        }
+
+        Article article = articleRepository.findById(id).get();
+
+        articleRepository.delete(article);
+
+        return String.format("%d번 삭제가 완료되었습니다.",id);
+    }
+
+    @RequestMapping("/doAdd")
+    @ResponseBody
+    public String doAdd(String title,String body,Long userId){
+        if(title == null){
+            return "title을 입력하세요.";
+        }
+
+        if(body == null) {
+            return "body를 입력하세요.";
+        }
+
+        if(userId == null) {
+            return "userId를 입력하세요.";
+        }
+
+
+        Article article = new Article();
+        article.setTitle(title);
+        article.setBody(body);
+
+        articleRepository.save(article);
+
+        return String.format("생성되었습니다.");
     }
 }
